8ea896bd98ca91c79811e32f9ec477a0c649ebb7
FrederichRiver <friederich@163.com>
2020-05-03 01:22:25 +0800

    3rd May, 2020

--- applications/dist/orion/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class stock_data(models.Model):
-    trade_date = models.DateField()
-    close_price = models.FloatField()
--- applications/dist/orion/neutrino/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-import pymysql
-
-pymysql.install_as_MySQLdb()
